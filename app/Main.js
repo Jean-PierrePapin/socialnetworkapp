3d6232982cf6d6d045154cabfd81f4a7228b4996
Jean-PierrePapin <skouo82@gmail.com>
2021-11-11 08:55:26 +0000

    Create post screen

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
 // Import components
 import Header from "./components/Header";
@@ -9,6 +11,7 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Terms from "./components/Terms";
 import About from "./components/About";
+import CreatePost from "./components/CreatePost";
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
@@ -19,6 +22,9 @@ function Main() {
       <Switch>
         <Route path="/" exact>
           {loggedIn ? <Home /> : <HomeGuest />}
+        </Route>
+        <Route path="/create-post">
+          <CreatePost></CreatePost>
         </Route>
         <Route path="/about-us">
           <About />
