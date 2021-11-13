612608ada84e0365205e47e11306b51253f7f727
Jean-PierrePapin <skouo82@gmail.com>
2021-11-13 09:15:44 +0000

    useReducer and Context used in the same time

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -2,19 +2,19 @@ import React, { useEffect, useState, useContext } from "react";
 import Page from "./Page";
 import Axios from "axios";
 import { withRouter } from "react-router-dom";
-import ExampleContext from "../ExampleContext";
+import DispatchContext from "../DispatchContext";
 
 function CreatePost(props) {
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
-  const { addFlashMessage } = useContext(ExampleContext);
+  const appDispatch = useContext(DispatchContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
       const response = await Axios.post("/create-post", { title, body, token: localStorage.getItem("complexappToken") });
       // Redirect to the new post url
-      addFlashMessage("Congrat, new post");
+      appDispatch({ type: "flashMessage", value: "Congrats, new post created." });
       props.history.push(`/post/${response.data}`);
       console.log("New post created");
     } catch (e) {
